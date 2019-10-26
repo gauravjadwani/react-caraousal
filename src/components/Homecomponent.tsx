b9b9f92907924e1371c0b516828206b16c3933d7
GAURAV JADWANI <gauravjadwani@licious.com>
2019-10-26 11:40:55 +0530

    initial sctructur

---
+++ src/components/Homecomponent.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import List from './List';
+
+class CustomTable extends React.Component<
+  any,
+  {
+  }
+> {
+//   constructor(props: any) {
+//     super(props);
+//     this.state = {
+//       chancesRemaining: CHANCES,
+//       error: '',
+//       message: 'playing',
+//       tInterval: 0,
+//       time: '00:00:00',
+//       value: randomise(40),
+//     };
+//   }
+  public render() {
+    return (
+      <div className="HomeComponent">
+          <List/>
+      </div>
+    );
+  }
+}
+export default CustomTable;
